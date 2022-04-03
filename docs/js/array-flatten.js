924208679aafe1b91960063bfaf4b050fab25568
xinconan <xinconan2@sina.com>
2022-04-03 21:21:28 +0800

    add js test

---
+++ docs/js/array-flatten.js
@@ -0,0 +1,23 @@
+function flatten(arr, deep = false) {
+  let res = [];
+  arr.forEach((item) => {
+    if (Array.isArray(item)) {
+      let val = item;
+      if (deep) {
+        val = flatten(val, true);
+      }
+      // 方法一
+      // val.forEach((v) => {
+      //   res.push(v);
+      // });
+      // 方法二
+      res = res.concat(val);
+    } else {
+      res.push(item);
+    }
+  });
+  return res;
+}
+
+console.log(flatten([1, 2, [3, ['a', 'b']], 5]));
+console.log(flatten([1, 2, [3, ['a', 'b']], 5], true));

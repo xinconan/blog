d5c581cc3d362620f1484788592bc76793844420
xinconan <xinconan2@sina.com>
2022-04-17 16:12:27 +0800

    test: webpack

---
+++ docs/js/webpack.js
@@ -0,0 +1,11 @@
+
+const webpack = (options, callback) => {
+  const create = () => {};
+  if(callback) {
+    const { compiler } = create();
+    return compiler;
+  } else {
+    const { compiler } = create();
+    return compiler;
+  }
+};
